5e393123a15f57e5dd6081b8e78a5dcce9e9af64
furkankarakocmobilist <furkan.karakoc@mobilistbilisim.com>
2023-01-14 01:44:45 +0300

    add alert pop-up & signIn control

--- AlertProject/ViewController.swift
+++ AlertProject/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var userNameText: UITextField!
+    
+    @IBOutlet weak var passwordText: UITextField!
+    
+    @IBOutlet weak var passwordAgainText: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func signupClicked(_ sender: Any) {
+        
+        if userNameText.text == "" {
+            makeAlert(titleInput: "Alert", messageInput: "Username not found")
+        } else if passwordText.text == "" || passwordAgainText.text == "" {
+            makeAlert(titleInput: "Alert", messageInput: "Password not found")
+        } else if passwordText.text != passwordAgainText.text {
+            makeAlert(titleInput: "Opps!", messageInput:"Passwords are not the same")
+        } else {
+            makeAlert(titleInput: "Success", messageInput: "Let's Sign")
+        }
+    }
+    
+    func makeAlert (titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+    }
 }
-
